fba40e7fd1a730114f3642b272250abb5e17e5cd
Nadezhda Svezhintseva <svezhintzeva@ro46.fss.ru>
2018-05-14 11:56:47 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar
+      absolute
+      color="teal lighten-3"
+      dark
+      scroll-off-screen
+      scroll-target="#scrolling-techniques"
+    >
+      <v-toolbar-title>КОНКУРСЫ</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Название нового конкурса</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
+
